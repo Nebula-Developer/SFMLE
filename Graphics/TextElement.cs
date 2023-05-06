8c51835f4dce6fe2a12cd50e420242886ef4e806
NebulaDev <nebuladev.contact@gmail.com>
2023-05-06 14:14:33 +1200

    Update text scaling to support new classes

--- Graphics/TextElement.cs
+++ Graphics/TextElement.cs
@@ -3,19 +3,35 @@ using SFML.System;
 
 namespace SFMLE;
 
-public class TextElement : TransformableElement {
+public class TextElement : TextTransformableElement {
     public Text Text { get; set; }
-    public TextElement() : base() {
-        Text = new Text("", new Font("/Library/Fonts/Arial Unicode.ttf"), 16) {
+
+    public TextElement() : base(null) {
+        Text = new Text("", new Font("/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf"), 16) {
             FillColor = Color.White
         };
+
+        this.element = this;
     }
     
     public new void Update() {
         base.Update();
         Text.Position = WorldPosition;
-        Text.Scale = Size;
         Text.Origin = new Vector2f(0, 0);
+
+        Text.LetterSpacing = 1;
+        int letterCount = Text.DisplayedString.Length;
+
+        float width = Text.GetLocalBounds().Width;
+        float height = Text.GetLocalBounds().Height;
+
+        Vector2f targetSize = Size;
+
+        float scaleX = targetSize.X / width;
+        float scaleY = targetSize.Y / height;
+
+        float scale = Math.Min(scaleX, scaleY);
+        Text.Scale = new Vector2f(scale, scale);
     }
 
     public void Draw(RenderTarget target, RenderStates states) {
--- Graphics/TransformableElement.cs
+++ Graphics/TransformableElement.cs
@@ -13,3 +13,14 @@ public class TransformableElement : Anchored, Element {
         else CalcWorldPosition(Parent.WorldPosition, Parent.Size);
     }
 }
+
+public class TextTransformableElement : TextAnchored, Element {
+    public TransformableElement Parent { get; set; }
+
+    public TextTransformableElement(TextElement element) : base(element) { }
+    
+    public void Update() {
+        if (Parent == null) CalcWorldPosition(new Vector2f(0, 0), new Vector2f(0, 0));
+        else CalcWorldPosition(Parent.WorldPosition, Parent.Size);
+    }
+}
